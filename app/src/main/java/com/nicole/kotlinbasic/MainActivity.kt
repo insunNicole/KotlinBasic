13a13e867e3f5f679dca5d3dbd97bf40a5a13978
insunNicole <florachoi1124@naver.com>
2020-08-05 20:38:05 +0900

    클릭 버튼이 클릭 되면 로그 찍기

--- app/src/main/java/com/nicole/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/nicole/kotlinbasic/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.nicole.kotlinbasic
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // 먼저 이 코틀린 파일이 실행될 view 파일을 지정을 해준다.
+
+
+//        ctrl + / : 해당 줄을 주석으로
+//        메모 처럼 주석을 적자
+//        xml에 그려둔 clickme 버튼이 눌렸다는 것을 감지 하고 싶다.
+
+        clickMeBtn.setOnClickListener {
+            // 중괄호 내부의 코드 => clickMeBtn이 눌리면 실행될 코드.
+            Log.d("버튼 클릭", "버튼이 클릭되었습니다.")
+        }
     }
 }
