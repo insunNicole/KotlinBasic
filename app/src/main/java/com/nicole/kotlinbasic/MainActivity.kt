19077dc2bfbd557e52f313c4d6b8cec6dd37b62a
insunNicole <florachoi1124@naver.com>
2020-08-05 20:13:49 +0900

    Initial commit

---
+++ app/src/main/java/com/nicole/kotlinbasic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nicole.kotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
